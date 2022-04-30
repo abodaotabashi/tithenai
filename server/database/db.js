0c29014ece4e5238e8cb4c0d6df8470593ebe789
Muhammed Şihebi <58932402+Nour0700@users.noreply.github.com>
2022-04-30 18:09:37 +0300

    Start using consistent names

--- server/database/db.js
+++ server/database/db.js
@@ -17,12 +17,61 @@ const db = admin.firestore();
 const storage = firebaseStorage.getStorage(firebseApp);
 
 //=========================================================== collection names 
-const USERS = 'users';
-const UNIS = 'universities';
-const THESES = 'theses';
-const REPORTS = 'reports';
-const REVIEWS = 'reviews';
-const BUCKETNAME = 'tithenai-23867.appspot.com'
+const USERS_COLLECTION = 'users';
+const UNIS_COLLECTION = 'universities';
+const THESES_COLLECTION = 'theses';
+const REPORTS_COLLECTION = 'reports';
+const REVIEWS_COLLECTION = 'reviews';
+const BUCKETNAME_COLLECTION = 'tithenai-23867.appspot.com'
+
+// =========================================================== Database Strings 
+// Reporsts 
+const REPORT_CONTENT = "reportContent"
+const REPORT_REPORTER_ID = "reportReporterId"
+const REPORT_THESIS_ID = "reportThesisId"
+const REPORTER_NAME = "reporterName"
+const REPORT_ID = "reportId"
+
+// Reviews 
+const REVIEW_AUTHOR_ID = "reviewAuthorId"
+const REVIEW_AUTHOR_NAME = "reviewAuthorName"
+const REVIEW_BODY = "reviewBody"
+const REVIEW_RATING = "reviewRating"
+const REVIEW_THESIS_ID = "reviewThesisId"
+
+// Theses 
+const THESIS_ABSTRACT = "thesisAbstract"
+const THESIS_AUTHER_ID = "thesisAutherId"
+const THESIS_AUTHOR_NAME = "thesisAuthorName"
+const THESIS_DATE = "thesisDate"
+const THESIS_FACULTY = "thesisFaculty"
+const THESIS_LANGUAGE = "thesisLanguage"
+const THESIS_PDF_URL = "thesisPdfUrl"
+const THESIS_TAGS = "thesisTags"
+const THESIS_TITLE = "thesisTitle"
+const THESIS_UNI_ID = "thesisUniId"
+const THESIS_UNI_NAME = "thesisUniName"
+const THESIS_UPLOAD_DATE = "thesisUploadDate"
+const THESIS_ID = "thesisId"
+
+// Universities
+const UNI_COUNTRY = "uniCountry"
+const UNI_NAME = "uniName"
+const UNI_STATE = "uniState"
+const UNI_THESES = "uniTheses"
+const UNI_TYPE = "uniType"
+const UNI_URL = "uniUrl"
+const UNI_ID = "uniId"
+
+// Users 
+const USER_LASTNAME = "userLastname"
+const USER_FIRSTNAME = "userFirstname"
+const USER_UNI_ID = "userUniId"
+const USER_ACADEMIC_STATUS = "userAcademicStatus"
+const USER_GENDER = "userGender"
+const USER_ADMIN = "userAdmin"
+const USER_ID = "uid"
+const USER_SAVED_THESES = "userSavedTheses"
 
 const DEFAULT_UNI = {
     uniCountry: "",
@@ -30,7 +79,8 @@ const DEFAULT_UNI = {
     uniState: "",
     uniTheses: "",
     uniType: "",
-    uniUrl: ""
+    uniUrl: "", 
+    uniID: ""
 }
 // =========================================================== Manage Users
 
@@ -38,7 +88,7 @@ async function addNewUser(data) {
     uid = data.uid
     idToken = data.idToken
     dbUserData = {
-        userUniversityID: data.userdata.userUniversityID,
+        userUniID: data.userdata.userUniversityID,
         userAcademicStatus: data.userdata.userAcademicStatus,
         userFirstname: data.userdata.userFirstname,
         userLastname: data.userdata.userLastname,
@@ -46,18 +96,19 @@ async function addNewUser(data) {
         userTheses: [], // first time adding a user, no theses yet.
         userAdmin: false,
         userReports: [],
+        userSavedTheses: []
     }
 
     // When user try to authenticate with google, they might exist in the database
     // Check if the user exist before adding any new data
 
     return db
-        .collection(USERS)
+        .collection(USERS_COLLECTION)
         .doc(uid)
         .get()
         .then((doc) => {
             return db
-                .collection(USERS)
+                .collection(USERS_COLLECTION)
                 .doc(uid)
                 .set(dbUserData)
                 .then(() => true)
@@ -72,32 +123,36 @@ async function addNewUser(data) {
 }
 
 async function getUserInfo(uid) {
-    return db.collection(USERS).doc(uid)
+    return db.collection(USERS_COLLECTION).doc(uid)
         .get()
         .then(async (doc) => {
             const authUserData = await admin.auth().getUser(uid)
             const firebaseUserData = doc.data()
-            const uniID = firebaseUserData.userUniversityID
-            const userThesesIds = firebaseUserData.userTheses
+            const uniID = firebaseUserData.userUniID
 
             // getting the uni of the user
             let uni = DEFAULT_UNI
             if (uniID) {
-                const uniSnapshot = await db.collection(UNIS).doc(uniID).get();
+                const uniSnapshot = await db.collection(UNIS_COLLECTION).doc(uniID).get();
                 uni = {
                     uniId: uniID,
                     ...uniSnapshot.data()
                 }
             }
-
+            // TODO: change this 
             // getting the theses of the user
-            let theses = [];
-            for (const thesisId of userThesesIds) {
-                const thesissnapshot = await db.collection(THESES).doc(thesisId).get();
-                theses.push(thesissnapshot.data())
-            }
 
-            delete firebaseUserData.uniId
+            const thesesSnapshot = await db.collection(THESES_COLLECTION).where("thesisAutherID", '==', uid).get();
+            const theses = []
+            thesesSnapshot.forEach(thesisObj => {
+                const thesis = {
+                    ...thesisObj.data(),
+                    thesisId: thesisObj.id
+                }
+                theses.push(thesis);
+            });
+
+            delete firebaseUserData.userUniID
             delete firebaseUserData.userTheses
 
             const userInfo = {
@@ -105,7 +160,8 @@ async function getUserInfo(uid) {
                 userEmail: authUserData.email,
                 userPhotoURL: authUserData.photoURL,
                 userUniversity: uni,
-                userTheses: theses
+                userTheses: theses, 
+                userId: uid
             }
             return userInfo;
         }).catch((error) => {
@@ -125,7 +181,7 @@ function deleteAllUsers(nextPageToken) {
             admin.auth().deleteUsers(uids);
         });
 
-    db.collection(USERS)
+    db.collection(USERS_COLLECTION)
         .get()
         .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
@@ -149,23 +205,23 @@ async function updateUser(newUserData) {
 
     try {
         // 1. update user data in the users collcection
-        await db.collection(USERS)
+        await db.collection(USERS_COLLECTION)
             .doc(newUserData.uid)
             .update({
                 userFirstname: newUserData.userFirstname,
                 userLastname: newUserData.userLastname,
                 userAcademicStatus: newUserData.userAcademicStatus,
-                userUniversityID: newUserData.userUniversityID
+                userUniID: newUserData.userUniversityID
             })
 
         // 2. update user name in reports collection
-        await updateNameInCollection(REPORTS, newUserData.uid, 'reportReporterID', "reporterName");
+        await updateNameInCollection(REPORTS_COLLECTION, newUserData.uid, 'reportReporterID', "reporterName");
 
         // 3. update user name in reviews collection
-        await updateNameInCollection(REVIEWS, newUserData.uid, 'reviewAuthorID', "reviewAuthorName");
+        await updateNameInCollection(REVIEWS_COLLECTION, newUserData.uid, 'reviewAuthorID', "reviewAuthorName");
 
         // 4. update user name in theses collection 
-        await updateNameInCollection(THESES, newUserData.uid, 'thesisAutherID', "thesisAuthorName");
+        await updateNameInCollection(THESES_COLLECTION, newUserData.uid, 'thesisAutherID', "thesisAuthorName");
 
         // 5. update user data in auth
         admin.auth()
@@ -186,7 +242,7 @@ async function updateUserImage(data) {
         const uid = data.uid;
         const imageBase64 = formatBase64(data.imageBase64)
         const buf = new Buffer.from(imageBase64, 'base64');
-        const file = storage.bucket(BUCKETNAME).file(`userImages/${uid}.png`);
+        const file = storage.bucket(BUCKETNAME_COLLECTION).file(`userImages/${uid}.png`);
         await file.save(buf);
         const publicUrl = await file.getSignedUrl({
             action: 'read',
@@ -203,35 +259,23 @@ async function updateUserImage(data) {
     }
 }
 
-async function getUserTheses(uid) {
-    try {
-        // TODO: change sK6ZvwH30gX1L0nQ4VQzCuF5sC02 to uid
-        // Note: don't use ctrl+right click to access the url from visual code, instead copy it
-        const thesesSnapshot = await db.collection(THESES).where("thesisAutherID", '==', "sK6ZvwH30gX1L0nQ4VQzCuF5sC02").get();
-        const theses = []
-        thesesSnapshot.forEach( thesisObj => {
-            theses.push(thesisObj.data()); 
-        });
-        return(theses)
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
-    return true
+async function saveThesis(data) {
+    const uid = data.uid;
+    const thesisId = data.thesisId
 }
 
 // =========================================================== Universities
 
 async function getAllUnis() {
     try {
-        const querySnapshot = await db.collection(UNIS)
+        const querySnapshot = await db.collection(UNIS_COLLECTION)
             .orderBy("uniName", "asc")
             .get();
 
         unis = []
         let counter = 0;
         querySnapshot.forEach((doc) => {
-            const uniImgRef = storage.bucket(BUCKETNAME).file(`uniImages/${doc.id}.png`)
+            const uniImgRef = storage.bucket(BUCKETNAME_COLLECTION).file(`uniImages/${doc.id}.png`)
             const uniImageUrl = uniImgRef.publicUrl();
             uni = {
                 ...doc.data(),
@@ -250,7 +294,7 @@ async function getAllUnis() {
 async function uploadUniImages() {
     fileNames = fs.readdirSync('database/uniImages/');
     fileNames.forEach(async (filename) => {
-        await storage.bucket(BUCKETNAME).upload("database/uniImages/" + filename, {
+        await storage.bucket(BUCKETNAME_COLLECTION).upload("database/uniImages/" + filename, {
             destination: "uniImages/" + filename,
         });
     });
@@ -260,7 +304,7 @@ async function uploadUniImages() {
 
 async function getAllTheses() {
     return db
-        .collection(THESES)
+        .collection(THESES_COLLECTION)
         .orderBy("thesisTitle", "asc")
         .get()
         .then((querySnapshot) => {
@@ -280,20 +324,22 @@ async function getAllTheses() {
 }
 
 async function uploadThesis(data) {
-    const uid = data.thesisAutherID;
-    // const thesisPdfBase64 = data.thesisPdfBase64;
-    const thesis = { ...data, thesisPdfUrl: "" };
-    thesis.thesisUploadDate = new Date(thesis.thesisUploadDate)
-    thesis.thesisDate = new Date(thesis.thesisDate)
-    delete thesis.thesisPdfBase64;
+    const thesisData = { ...data, thesisPdfUrl: "" }
+    const uid = thesisData.thesisAutherID;
+    const thesisPdfBase64 = thesisData.thesisPdfBase64;
+    delete thesisData.uid;
+    delete thesisData.thesisPdfBase64;
+    thesisData.thesisUploadDate = new Date(thesisData.thesisUploadDate)
+    thesisData.thesisDate = new Date(thesisData.thesisDate)
+
     try {
         // add new thesis 
-        const addedThesis = await db.collection(THESES).add(thesis)
+        const addedThesis = await db.collection(THESES_COLLECTION).add(thesis)
 
         // TODO: get a real pdf file as Base64
         const pdfFile = await fs.readFileSync('database/myThesis.pdf', 'base64');
         const buf = new Buffer.from(pdfFile, 'base64');
-        const file = storage.bucket(BUCKETNAME).file(`theses/${addedThesis.id}.pdf`);
+        const file = storage.bucket(BUCKETNAME_COLLECTION).file(`theses/${addedThesis.id}.pdf`);
         await file.save(buf);
         const publicUrl = await file.getSignedUrl({
             action: 'read',
@@ -301,7 +347,7 @@ async function uploadThesis(data) {
         });
 
         // update document with the url
-        await db.collection(THESES).doc(addedThesis.id).update({
+        await db.collection(THESES_COLLECTION).doc(addedThesis.id).update({
             thesisPdfUrl: publicUrl
         })
 
@@ -311,7 +357,6 @@ async function uploadThesis(data) {
     }
     return true
 }
-
 
 // =========================================================== Exports
 
@@ -324,4 +369,3 @@ module.exports.getUserInfo = getUserInfo;
 module.exports.uploadUniImages = uploadUniImages;
 module.exports.updateUserImage = updateUserImage;
 module.exports.uploadThesis = uploadThesis;
-module.exports.getUserTheses = getUserTheses;
