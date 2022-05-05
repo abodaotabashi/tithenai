9f66787df81f7cc209704faa2e14bdefdcffd28d
Abdurrahman Odabaşı <60965447+abodaotabashi@users.noreply.github.com>
2022-05-05 12:41:29 +0300

    Landing Page edited

--- client/src/pages/MainPages/LandingPage.js
+++ client/src/pages/MainPages/LandingPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { CssBaseline, Paper } from '@mui/material';
+import { CssBaseline, Grid, Paper } from '@mui/material';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
-import { redirectToMainPage, redirectToLoginPage, redirectToRegisterPage } from '../../utils/Redirecter';
+import { redirectToLoginPage, redirectToRegisterPage } from '../../utils/Redirecter';
 import { useNavigate } from 'react-router-dom';
 import { makeStyles } from '@mui/styles';
-import LoginGif from '../../assets/gifs/Login_300_35.gif';
+import LandingWelcomeGif from '../../assets/gifs/LandingWelcome.gif';
 
 import '../../assets/styles.css';
 const useStyles = makeStyles(theme => ({
@@ -26,26 +26,20 @@ const useStyles = makeStyles(theme => ({
         fontSize: "xx-large",
         fontFamily: "Ubuntu",
         color: "black",
-        paddingRight: "3rem",
         [theme.breakpoints.down('md')]: {
             fontSize: "x-large",
-            paddingRight: "1rem",
         },
         [theme.breakpoints.down('sm')]: {
             fontSize: "x-large",
-            paddingRight: "1rem",
         },
     },
     loginIllustration: {
-        width: "18rem",
+        width: "16rem",
         [theme.breakpoints.down('md')]: {
-            width: "14rem",
+            width: "12rem",
         },
         [theme.breakpoints.down('sm')]: {
             width: "10rem",
-        },
-        [theme.breakpoints.down('xs')]: {
-            width: "6rem",
         },
     }
 }));
@@ -58,24 +52,32 @@ const LandingPage = () => {
             <div style={{padding: "20px", marginTop: "10px", display: "flex", justifyContent: "center"}}>
                 <Paper style={{width: "80%", padding: "50px"}}>
                     <p className={classes.illustrationLabel}>Welcome to Tithenai</p>
-                   { <img src={LoginGif} alt="Welcome" className={classes.loginIllustration}/>}
-                    <p className={classes.header}>Get inspired by others' thesis, publish your own one and share your thoughts with your colleages.</p>
-                    <p className={classes.headerSpan}>
-                        To start, 
-                        <span className="forwardingSpan" onClick={() => redirectToRegisterPage(navigator)}>&nbsp;Sign Up Now!</span>
-                    </p>
-                    <p className={classes.headerSpan}>
-                        Or  
-                        <span className="forwardingSpan" onClick={() => redirectToLoginPage(navigator)}>&nbsp;Sign In </span>
-                        if you already have an account :)
-                    </p>
-                    <p className={classes.headerSpan}>
-                        To learn more, 
-                        <span className="forwardingSpan" to="aboutTithenai" onClick="aboutTithenai">&nbsp;click here.</span>
-                    </p>
+                    <Grid container alignItems="center">
+                        <Grid item xs={12} sm={12} md={5} lg={4}>
+                            <img src={LandingWelcomeGif} alt="Welcome" className={classes.loginIllustration}/>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={7} lg={8}>
+                            <p className="textWithSecondaryGradient">Get inspired by others' thesis, publish your own one and share your thoughts with your colleages.</p>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={12} lg={12}>
+                            <p className={classes.headerSpan}>
+                                To start,
+                                <span className="forwardingSpan" onClick={() => redirectToRegisterPage(navigator)}>&nbsp;Sign Up Now!</span>
+                            </p>
+                            <p className={classes.headerSpan}>
+                                Or
+                                <span className="forwardingSpan" onClick={() => redirectToLoginPage(navigator)}>&nbsp;Sign In </span>
+                                if you already have an account :)
+                            </p>
+                            <p className={classes.headerSpan}>
+                                To learn more,
+                                <span className="forwardingSpan" to="aboutTithenai" onClick="aboutTithenai">&nbsp;click here.</span>
+                            </p>
+                        </Grid>
+                    </Grid>
                 </Paper>
             </div>
-            <div style={{padding: "20px", marginTop: "10px", display: "flex", justifyContent: "center"}}>
+            <div style={{padding: "20px", display: "flex", justifyContent: "center"}}>
                 <Paper style={{width: "80%", padding: "50px"}} id="aboutTithenai">
                     <p className={classes.illustrationLabel}>About Tithenai</p>
                     <p className={classes.header}>
