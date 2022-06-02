509f03747f7783da0a3e730ec78a2f0603d8c2cc
Muhammed Sihebi <58932402+muhammed-shihebi@users.noreply.github.com>
2022-06-03 01:50:28 +0300

    Create send email util function

--- server/Utils/util.js
+++ server/Utils/util.js
@@ -1,38 +1,71 @@
+const nodemailer = require('nodemailer');
+
 // test 
 // console.log(turkishtoEnglish('ğğĞĞÜÜüüŞŞşşÖÖööÇÇçç'))
-function turkishtoEnglish (str) {
+function turkishtoEnglish(str) {
     return str
         .replace(/Ğ/g, "G")
-        .replace(/ğ/g, "g") 
-        .replace(/Ü/g, "U") 
-        .replace(/ü/g, "u") 
-        .replace(/Ş/g, "S") 
-        .replace(/ş/g, "s") 
-        .replace(/İ/g, "I") 
-        .replace(/ı/g, "i") 
+        .replace(/ğ/g, "g")
+        .replace(/Ü/g, "U")
+        .replace(/ü/g, "u")
+        .replace(/Ş/g, "S")
+        .replace(/ş/g, "s")
+        .replace(/İ/g, "I")
+        .replace(/ı/g, "i")
         .replace(/Ö/g, "O")
-        .replace(/ö/g, "o") 
-        .replace(/Ç/g, "C") 
+        .replace(/ö/g, "o")
+        .replace(/Ç/g, "C")
         .replace(/ç/g, "c");
 };
 
 
 // sortAlphabetically("uniName", "tr"); 
-function sortAlphabetically (property, languageCode){
-    return function(a, b) {
+function sortAlphabetically(property, languageCode) {
+    return function (a, b) {
         return a[property].localeCompare(b[property], languageCode)
     }
 }
 
-function formatBase64(base64File){
+function formatBase64(base64File) {
     return base64File.replace(/^data:image\/\w+;base64,/, "");
 }
 
-function formatFirebaseDate (date, format) {
-    if(format === "year"){
+function formatFirebaseDate(date, format) {
+    if (format === "year") {
         return new Date(date._seconds * 1000).getFullYear().toString()
-    }else if (format === "month"){
+    } else if (format === "month") {
         return String(new Date(date._seconds * 1000).getMonth() + 1).padStart(2, '0')
+    }
+}
+
+/**
+ * @param {*} receiver is the email of the receiver 
+ * @param {*} content is the content of the email, which can be plain text or html
+ * @returns "email was not sent" or "email was sent"
+ * @example sendEmail("yourmail@google.com", "<h>hi this is a test</h>")
+ */
+
+async function sendEmail(receiver, content) {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+            user: "tithenai@outlook.com",
+            pass: "4mQpMdMYba6myjkT",
+        },
+    });
+    const options = {
+        from: 'tithenai@outlook.com',
+        to: receiver,
+        subject: "Tithenai",
+        text: "",
+        html: content,
+    }
+    const info = await transporter.sendMail(options);
+    if(info.rejected.length !== 0){
+        return "email was not sent"
+    }else{
+        return "email was sent"
     }
 }
 
