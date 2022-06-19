61aba5175085eb52d0c1eb416ca286f75527b380
Muhammed Sihebi <58932402+muhammed-shihebi@users.noreply.github.com>
2022-06-19 13:49:15 +0300

    Fix api problem

--- client/src/Service.js
+++ client/src/Service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASEURL = "api";
+const BASEURL = "/api";
 axios.defaults.baseURL = BASEURL
 // =========================================================== Utils
 
